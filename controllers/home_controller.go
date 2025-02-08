734ee6981e37c2582f9ee300632ec00d92dcec5a
Sachin Talekar <sachintalekar@ip-192-168-0-111.ap-south-1.compute.internal>
2025-02-08 14:10:26 +0530

    air installed

--- controllers/home_controller.go
+++ controllers/home_controller.go
@@ -4,5 +4,6 @@ import "github.com/gin-gonic/gin"
 
 // HomeController handles the home route
 func HomeController(c *gin.Context) {
-    c.JSON(200, gin.H{"message": "Welcome to Go Boilerplate!"})
+	println("HomeController")
+	c.JSON(200, gin.H{"message": "Welcome to Go Boilerplate!"})
 }
--- routes/routes.go
+++ routes/routes.go
@@ -1,11 +1,19 @@
 package routes
 
 import (
-    "github.com/gin-gonic/gin"
-    "go-gin-boilerplate/controllers"
+	"go-gin-boilerplate/controllers"
+
+	"github.com/gin-gonic/gin"
 )
 
 // InitRoutes initializes all routes
+// InitRoutes initializes the routes for the given gin engine.
+// It sets up the route for the home controller.
+//
+// Parameters:
+//   - r: A pointer to the gin.Engine instance where the routes will be registered.
 func InitRoutes(r *gin.Engine) {
-    r.GET("/", controllers.HomeController)
+	println("InitRoutes")
+	println("InitRoutes", r)
+	r.GET("/", controllers.HomeController)
 }
