3eb02826c1b4d7f01fea784f5bc6d90f705e9bf1
Sachin Talekar <sachintalekar@Sachins-MacBook-Pro-M4.local>
2025-03-30 21:10:05 +0530

    stablized

--- routes/routes.go
+++ routes/routes.go
@@ -1,28 +1,12 @@
 package routes
 
 import (
-	"go-gin-boilerplate/controllers"
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
-// InitRoutes initializes all routes
-func InitRoutes(route *gin.Engine) {
-
-	// Group for API Home and Health Check
-	DefaultApis := route.Group("default")
-	{
-		// Home Page
-		DefaultApis.GET("/", func(c *gin.Context) { controllers.DefaultPage(c) })
-		// Health-check route
-		DefaultApis.GET("/health-check", func(c *gin.Context) { controllers.HealthCheck(c) })
-	}
-
-	route.NoRoute(func(c *gin.Context) {
-		println("Fallback route triggered for:", c.Request.URL.Path)
-		c.JSON(http.StatusNotFound, gin.H{"message": "Route not found"})
-	})
-
-	println("InitRoutes completed")
+// SetupRoutes initializes all route groups
+func InitRoutes(router *gin.Engine) {
+	// Define the DefaultApis route group
+	AuthRoutes(router)
+	DefaultRoutes(router)
 }
